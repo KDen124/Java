08046b14d9e71189cd32ee3a407c242cc3ee0dcd
KDen124 <kaydenb8@nycstudents.net>
2025-10-21 13:32:58 +0000

    finish

--- Lesson-12-Activity/Main.java
+++ Lesson-12-Activity/Main.java
@@ -15,15 +15,16 @@ class Main {
     return "Visa";
    } else if (cardNumber.substring(0, 2).equals("34") || cardNumber.substring(0, 2).equals("37")) {
     return "American Express";
-   } else if (cardNumber.substring(0, 2).equals("36") 
+   } else if (cardNumber.substring(0, 2).equals("36")){ 
     return "Diner Club";
    } else if (cardNumber.substring(0, 2).equals("51") || cardNumber.substring(0, 2).equals("55")) {
     return "MasterCard";
-   } else if (cardNumber.substring(0, 4).equals("6011") || cardNumber.substring(0, 2).equals("65") ||
+   } else if (cardNumber.substring(0, 4).equals("6011") || cardNumber.substring(0, 2).equals("65")){
     return "Discovery";
    } else {
     return "Unknown Card Type";
- }
+   }
+  }
 
  String pigLatin(String word){
   String firstLetter = word.substring(0, 1);
@@ -31,15 +32,15 @@ class Main {
   return restOfWord + firstLetter + "ay";
  }
  String nycLocate(String zip){
-  if zip.substring(0,3).equals("100") || zip.substring(0,3).equals("101") || zip.substring(0,3).equals("102") {
+  if (zip.substring(0,3).equals("100") || zip.substring(0,3).equals("101") || zip.substring(0,3).equals("102")) {
    return "Manhattan";
-  } else if zip.substring(0,3).equals("103") {
+  } else if (zip.substring(0,3).equals("103")) {
    return "Staten Island";
-  } else if zip.substring(0,3).equals("104") {
+  } else if (zip.substring(0,3).equals("104")) {
    return "The Bronx";
-  } else if zip.substring(0,3).equals("112") {
+  } else if (zip.substring(0,3).equals("112")) {
    return "Brooklyn";
-  } else if zip.substring(0,3).equals("111") || zip.substring(0,3).equals("113") || zip.substring(0,3).equals("114") {
+  } else if (zip.substring(0,3).equals("111") || zip.substring(0,3).equals("113") || zip.substring(0,3).equals("114")) {
    return "Queens";
   } else {
    return "Unknown Borough";
