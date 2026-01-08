31c31bf7fb0c2afe152713b4951b4705f20a98a6
KDen124 <kaydenb8@nycstudents.net>
2026-01-08 14:24:39 +0000

    constructed

--- Lesson-30-Activity/Lesson-30-Activity/Main.java
+++ Lesson-30-Activity/Lesson-30-Activity/Main.java
@@ -8,20 +8,12 @@ class Main {
   void printt(Object o){System.out.print(o);}
 
   void init(){
-    Car Car1 = new Car();
-    Car1.brand = "Toyota";
-    Car1.color = "Red";
-    Car1.model = "Corolla";
-    Car1.year = 2020;
-    Car1.value = 20000.00;
+    Car Car1 = new Car("Toyota", "Red", "Corolla", 2020, 20000.00);
+
     Car1.honk();
 
-    Car Car2 = new Car();
-    Car2.brand = "Honda";
-    Car2.color = "Blue";    
-    Car2.model = "Civic";
-    Car2.year = 2019;
-    Car2.value = 22000.00;
+    Car Car2 = new Car("Honda", "Blue", "Civic", 2019, 22000.00);
+
     Car2.honk();
 
   }
@@ -32,6 +24,14 @@ class Main {
     String model;
     int year;
     double value;
+
+    Car(String brand, String color, String model, int year, double value) {
+      this.brand = brand;
+      this.color = color;
+      this.model = model;
+      this.year = year;
+      this.value = value;
+    }
 
     void honk(){
       System.out.println("Beep! Beep!");
