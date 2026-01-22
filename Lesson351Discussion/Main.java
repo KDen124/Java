2a7b383dd3bbc68b24e2ac8a8b5c05b88dce4ec9
KDen124 <kaydenb8@nycstudents.net>
2026-01-22 14:34:06 +0000

    D

---
+++ Lesson351Discussion/Main.java
@@ -0,0 +1,58 @@
+class Main {
+  public static void main(String[] args)throws Exception {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init()throws Exception{
+
+    CreateList list=new CreateList("CR101.csv");
+    Student[] students = list.getStudentArray();
+    
+    
+    
+    
+    
+  }
+
+
+  double sum = 0;
+  for(int x=0; x<students.length; x++){
+    Student student = students[x];
+    sum += Student.gpa;
+  }
+  print ("School Avg:" + sum/students.length);
+
+
+  int count = 0;
+  for(int x=0; x<students.length; x++){
+    Student student = students[x];
+    if(student.findTeacher == ("Castro R")){
+      count++;
+    }
+  }
+  print("Number of students with Castro R as teacher: " + count);
+
+
+  int stud = 0;
+  for (int x=0; x<students.length; x++){
+    Student student = students[x];
+  
+
+  }
+  for (int x=0; x<students.length; x++){
+    Student student = students[x];
+    if(student.findTeacher("Porchetta")&& student.findCourseStartingWith("MK")){
+      print(student.id);
+    }
+  }
+
+
+
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+}
